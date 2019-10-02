206189caf21db0002d48becb8812417fe49f62fc
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-10-02 12:34:23 +0200

    Add styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './styles/global.scss';
+import 'animate.css/animate.min.css';
 
 const Root = () => (
   <App/>
