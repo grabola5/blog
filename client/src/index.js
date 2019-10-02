eca0209e5a2cf564e6693aaf1d50627379a6bfa0
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-10-02 21:29:35 +0200

    Add subpages

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './styles/global.scss';
 import 'animate.css/animate.min.css';
 
 const Root = () => (
-  <App/>
+  <Router>
+    <App/>
+  </Router>
 );
 
 ReactDOM.render(<Root />, document.getElementById('root'));
