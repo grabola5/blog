5dab8b0f1554b4d4eb221fb3e3a02389699aaade
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-10-03 11:16:51 +0200

    Add components AddPost and SinglePost

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,8 @@ import MainLayout from './components/layout/MainLayout/MainLayout';
 import Home from './components/pages/Home/HomePage';
 import Posts from './components/pages/Posts/PostsPage';
 import Contact from './components/pages/Contact/ContactPage';
+import AddPost from './components/pages/AddPost/AddPostPage';
+import SinglePost from './components/pages/SinglePost/SinglePostPage';
 import NotFound from './components/pages/NotFound/NotFoundPage';
 
 class App extends React.Component {
@@ -16,6 +18,8 @@ class App extends React.Component {
           <Route path="/" exact component={Home} />
           <Route path="/posts" exact component={Posts} />
           <Route path="/contact" exact component={Contact} />
+          <Route path="/posts/new" exact component={AddPost} />
+          <Route path="/posts/:id" exact component={SinglePost} />
           <Route component={NotFound} />
         </Switch>
       </MainLayout>
