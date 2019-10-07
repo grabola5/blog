19a32e26cfa76fd2c5b2e5b3cc9ec54643d8c13b
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-10-07 22:38:03 +0200

    Add loader

--- client/src/redux/postsRedux.js
+++ client/src/redux/postsRedux.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 import { API_URL } from '../config';
 
 /* SELECTORS */
-export const getPosts = ({ posts }) => posts;
-export const getPostsCounter = ({ posts }) => posts.length;
+export const getPosts = ({ posts }) => posts.data;
+export const getPostsCounter = ({ posts }) => posts.data.length;
+export const getRequest = ({ posts }) => posts.request;
 
 /* ACTIONS */
 
@@ -11,21 +12,34 @@ export const getPostsCounter = ({ posts }) => posts.length;
 const reducerName = 'posts';
 const createActionName = name => `app/${reducerName}/${name}`;
 export const LOAD_POSTS = createActionName('LOAD_POSTS');
+export const START_REQUEST = createActionName('START_REQUEST');
+export const END_REQUEST = createActionName('END_REQUEST');
+
 export const loadPosts = payload => ({ payload, type: LOAD_POSTS });
+export const startRequest = () => ({ type: START_REQUEST });
+export const endRequest = () => ({ type: END_REQUEST });
 
 /* INITIAL STATE */
 
-const initialState = [];
+const initialState = {
+  data: [],
+  request: {
+    pending: false, //informacja o statusie ładowania
+  },
+};
 
 /* THUNKS */
 export const loadPostsRequest = () => {
     return async dispatch => {
-
+        dispatch(startRequest());
         try {
             let res = await axios.get(`${API_URL}/posts`);
+            await new Promise((resolve, reject) => setTimeout(resolve, 2000));
             dispatch(loadPosts(res.data));
+            dispatch(endRequest());
+
         } catch (e) {
-            console.log(e.message);
+            dispatch(endRequest());
         }
     };
 };
@@ -35,7 +49,11 @@ export const loadPostsRequest = () => {
 export default function reducer(statePart = initialState, action = {}) {
     switch (action.type) {
         case LOAD_POSTS:
-            return [ ...action.payload ];
+            return { ...statePart, data: action.payload };
+        case START_REQUEST:
+            return { ...statePart, request: { pending: true } };
+        case END_REQUEST:
+          return { ... statePart, request: { pending: false } };
         default:
             return statePart;
     }
