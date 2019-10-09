68af887bc0c032d66bdf34373dd2c8b6d8898732
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-10-09 11:09:33 +0200

    Add Helmet

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const config = require('./config');
 const mongoose = require('mongoose');
 const loadTestData = require('./testData');
+const helmet = require('helmet');
 
 const app = express();
 
@@ -10,9 +11,11 @@ const app = express();
 const postRoutes = require('./routes/post.routes');
 
 app.use(cors());
+app.use(helmet());
 app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use('/api', postRoutes);
+
 
 // connects our back end code with the database
 mongoose.connect(config.DB, { useNewUrlParser: true });
