5031a94f00f2ff50a699c94cb778ed1e8c7ad22c
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-10-09 10:13:14 +0200

    Use MongoDB

--- server/server.js
+++ server/server.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
+const mongoose = require('mongoose');
 
 const app = express();
 
-//import routes
+// import routes
 const postRoutes = require('./routes/post.routes');
 
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true}));
 app.use(express.json());
 app.use('/api', postRoutes);
 
-app.listen(config.PORT, function(){
-  console.log('Server is running on port:', config.PORT);
-});
+// connects our back end code with the database
+mongoose.connect(config.DB, { useNewUrlParser: true });
+let db = mongoose.connection;
+
+db.once('open', () => console.log('Connected to the database'));
+db.on('error', (err) => console.log('Error ' + err));
+
+app.listen(config.PORT, function() {
+  console.log('Server is running on Port: ', config.PORT);
+}
